98f3af8181dfbfe8903edcd72da85d7e75bf2396
wabray17 <wabray17@gmail.com>
2016-09-24 18:54:09 -0500

    Updating functionality added

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -23,6 +23,19 @@ class LinksController < ApplicationController
     Visit.create(link_id: @link.first.id, ip_address: request.remote_ip)
 
     redirect_to @link.first.target_url
-    
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update(user_id: current_user.id,
+                 target_url: params[:target_url],
+                 slug: params[:slug])
+
+    redirect_to '/links'
+    flash[:success] = "Link updated"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get '/new' => 'links#new'
   post '/links' => 'links#create'
   get '/links/:slug' => 'links#show'
+  get '/links/:id/edit' => 'links#edit'
+  patch '/links/:id' => 'links#update'
+  delete '/links/:id' => 'links#destroy'
 
   get '/visits/:id' => 'visits#show'
 end
